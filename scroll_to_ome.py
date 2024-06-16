dfcb3092ffb447eae2f481b8904e356406aa708d
Yao Hsiao <tomtogo1260@gmail.com>
2024-06-16 19:23:12 +0800

    tiff folder don't need to exist when using first new level flag

--- scroll_to_ome.py
+++ scroll_to_ome.py
@@ -446,7 +446,7 @@ def main():
         return 1
     
     tiffdir = Path(args.input_tiff_dir)
-    if not tiffdir.exists():
+    if not tiffdir.exists() and args.first_new_level is None:
         print("Input TIFF directory",tiffdir,"does not exist")
         return 1
 
